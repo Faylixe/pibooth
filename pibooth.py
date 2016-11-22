e7e7f10027eeeb61dd1a5a59571d198261417dff
Faylixe <felix.voituret@gmail.com>
2016-11-22 16:34:47 +0100

    Start architecture

---
+++ pibooth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from ui import UI
+
+def onPicturePerformed():
+    """ Callback method that handles post picture processing. """
+    pass
+
+def onVideoPerformed():
+    """ Callback method that handles post video processing. """
+    pass
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
---
+++ ui.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import pygame
+
+pygame.init()
+
+class UI(object):
+    """ """
+
+    def __init__(self, size=(640, 480)):
+        """ """
+        self.window = pygame.display.set_mode(size)
+    
+    def run(self):
+        """ """
+        while self.running:
+            pass
